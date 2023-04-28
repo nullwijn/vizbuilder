418ae8cfa88936a507cf2dd14f6acab5a21f443e
nullwijn <56175027+nullwijn@users.noreply.github.com>
2023-04-27 23:15:06 -0400

    Update description

--- DESCRIPTION.R
+++ DESCRIPTION.R
@@ -4,7 +4,7 @@ Version: 0.1
 Authors@R: "Tristan Wijnmaalen <twijnmaalen@usf.edu> [aut, cre]"
 Description: This package aims to simplify ggplot2 to be approachable for
     the common user. The purpose is to help interface between ggplot2 on
-    backend and simple inputs on the front end. 
+    backend and simple inputs with shiny on the front end. 
 Depends: R (>= 3.1.2), ggplot2
 License: CC0
 LazyData: true
