3b8e39792197f7b44a187ddcc01d77e7d6297f48
nullwijn <56175027+nullwijn@users.noreply.github.com>
2023-04-27 23:49:49 -0400

    Update description, create scatterplot function

---
+++ R/scatterplot.R
@@ -0,0 +1,11 @@
+#' Visual Builder
+#'
+#' This function takes a data frame and creates a Shiny web app
+#' for you to create a visualization with.
+#' @param df The dataframe
+#' @keywords viz
+#' @export
+
+vizbuilder <- function(df){
+
+}
