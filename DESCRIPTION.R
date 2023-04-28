821b49d49d06dfda6491a9c0c5dd8e2e39787041
nullwijn <56175027+nullwijn@users.noreply.github.com>
2023-04-27 23:12:23 -0400

    Create DESCRIPTION.R

---
+++ DESCRIPTION.R
@@ -0,0 +1,10 @@
+Package: vizbuilder
+Title: Creates visualizations for you
+Version: 0.1
+Authors@R: "Tristan Wijnmaalen <twijnmaalen@usf.edu> [aut, cre]"
+Description: This package aims to simplify ggplot2 to be approachable for
+    the common user. The purpose is to help interface between ggplot2 on
+    backend and simple inputs on the front end. 
+Depends: R (>= 3.1.2), ggplot2
+License: CC0
+LazyData: true
