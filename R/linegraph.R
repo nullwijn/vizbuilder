820d0f8d98eb579d5f77270788eacd044fc13618
nullwijn <56175027+nullwijn@users.noreply.github.com>
2023-04-28 01:00:15 -0400

    Finish project

--- R/linegraph.R
+++ R/linegraph.R
@@ -1,13 +1,14 @@
-#' Visual Builder: Scatter plot
+#' Visual Builder: Line Graph
 #'
 #' This function takes a data frame and creates a shiny web app
-#' for you to create a scatter plot with.
-#' There are four customizable elements: x-axis, y-axis, color, and size
+#' for you to create a lien graph with.
+#' There are three customizable elements: x-axis, y-axis, and 2 colors
+#' In this function, colors are predetermined for a gradient
 #' @param df The data frame
 #' @keywords scatter plot
 #' @export
 
-scatterplot <- function(df){
+linegraph <- function(df){
   #Create the UI
   ui <- shiny::fluidPage(
     # Create a basic panel and sidebar with dropdown input selections
@@ -16,11 +17,10 @@ scatterplot <- function(df){
       shiny::sidebarPanel(
         shiny::selectInput(inputId = "x", label = "X-axis", choices = colnames(df)),
         shiny::selectInput(inputId = "y", label = "Y-axis", choices = colnames(df)),
-        shiny::selectInput(inputId = "color", label = "Color", choices = colnames(df)),
-        shiny::selectInput(inputId = "size", label = "Size", choices = colnames(df))
+        shiny::selectInput(inputId = "color1", label = "Color", choices = c("green","red","blue","black","grey"))
       ),
       shiny::mainPanel(
-        plotly::plotlyOutput(outputId = "scatterplot")
+        plotly::plotlyOutput(outputId = "linegraph")
       )
     )
   )
@@ -28,14 +28,15 @@ scatterplot <- function(df){
   server <- function(input, output) {
     # Create a reactive data frame to catch the inputs and put them into the plot
     data <- reactive({
-      df[, c(input$x, input$y, input$color, input$size)]
+      df[, c(input$x, input$y, input$color)]
     })
     # Create the scatterplot using ggplot and plotly
     # Input variables are passed to the server as strings in Shiny, so I need to use !!as.name to grab the actual value
-    output$scatterplot <- plotly::renderPlotly({
-      ggplot2::ggplot(data(), ggplot2::aes(x = !!as.name(input$x), y = !!as.name(input$y), color = !!as.name(input$color), size = !!as.name(input$size))) +
-        ggplot2::geom_point() +
-        ggplot2::labs(x = input$x, y = input$y, color = input$color, size = input$size) +
+    output$linegraph <- plotly::renderPlotly({
+      ggplot2::ggplot(data(), ggplot2::aes(x = !!as.name(input$x), y = !!as.name(input$y), color = input$y)) +
+        ggplot2::geom_line() +
+        ggplot2::labs(x = input$x, y = input$y) +
+        ggplot2::scale_color_manual(values = input$color1) +
         ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90)) #Make x-axis values vertical so that they are legible
     })
   }
